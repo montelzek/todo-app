5c8d247ca494b515fc8284845857fea047167cf3
montelzek <patrykmatras378@gmail.com>
2024-12-04 18:44:27 +0100

    Adde TasRepository interface

---
+++ src/main/java/com/montelzek/todoapp/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.montelzek.todoapp.repository;
+
+import com.montelzek.todoapp.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+}
