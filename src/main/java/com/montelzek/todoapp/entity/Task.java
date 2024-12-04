9f7f3c0fc2d5c2a3d6e4b731b6c2cc7b9aafd7b0
montelzek <patrykmatras378@gmail.com>
2024-12-04 16:14:02 +0100

    Created Entity class for Task

---
+++ src/main/java/com/montelzek/todoapp/entity/Task.java
@@ -0,0 +1,94 @@
+package com.montelzek.todoapp.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private int id;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "priority_id")
+    private Priority priority;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+
+    @Column(name = "is_completed", nullable = false)
+    private boolean isCompleted;
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public Task() {
+
+    }
+
+    public Task(String title, Priority priority, LocalDate dueDate, boolean isCompleted, LocalDateTime createdAt) {
+        this.title = title;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.isCompleted = isCompleted;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
