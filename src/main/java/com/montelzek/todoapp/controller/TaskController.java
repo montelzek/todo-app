1dc05b5a11c22c62407544881693ab0c32649722
montelzek <patrykmatras378@gmail.com>
2024-12-04 19:21:25 +0100

    Add endpoint to list tasks
    * Implements the `/list` endpoint to retrieve all tasks from the service.
    * Adds the retrieved tasks to the Model for display in the `list-tasks` view.

--- src/main/java/com/montelzek/todoapp/controller/TaskController.java
+++ src/main/java/com/montelzek/todoapp/controller/TaskController.java
@@ -1,9 +1,14 @@
 package com.montelzek.todoapp.controller;
 
+import com.montelzek.todoapp.entity.Task;
 import com.montelzek.todoapp.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/tasks")
@@ -14,5 +19,15 @@ public class TaskController {
     @Autowired
     public TaskController(TaskService theTaskService) {
         taskService = theTaskService;
+    }
+
+    @GetMapping("/list")
+    public String listTasks(Model theModel) {
+
+        List<Task> theTasks = taskService.findAll();
+
+        theModel.addAttribute("tasks", theTasks);
+
+        return "tasks/list-tasks";
     }
 }
