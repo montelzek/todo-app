304b00d99036312cb9883befcbaf0b8b8e8a81d0
montelzek <patrykmatras378@gmail.com>
2024-12-04 19:06:00 +0100

    Added @Service and @Autowired annotations

--- src/main/java/com/montelzek/todoapp/service/PriorityServiceImpl.java
+++ src/main/java/com/montelzek/todoapp/service/PriorityServiceImpl.java
@@ -2,13 +2,17 @@ package com.montelzek.todoapp.service;
 
 import com.montelzek.todoapp.entity.Priority;
 import com.montelzek.todoapp.repository.PriorityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PriorityServiceImpl implements PriorityService{
 
-    private PriorityRepository priorityRepository;
+    private final PriorityRepository priorityRepository;
 
+    @Autowired
     public PriorityServiceImpl(PriorityRepository thePriorityRepository) {
         priorityRepository = thePriorityRepository;
     }
